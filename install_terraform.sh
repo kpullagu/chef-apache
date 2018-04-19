6b7106ad5a98e46f673ed84eea005144f25a1599
Kranthi <kranthi.pullagurla@gmail.com>
2018-04-18 20:08:57 -0500

    Updating the Terraform installer version to 0.11.7 and the OCI provider plugin to 2.1.5

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,6 +1,6 @@
 #Get Terraform
-version=terraform_0.11.2_linux_amd64.zip
-wget "https://releases.hashicorp.com/terraform/0.11.2/$version"
+version=terraform_0.11.7_linux_amd64.zip
+wget "https://releases.hashicorp.com/terraform/0.11.7/$version"
 unzip $version -d ~/
 rm $version
 cd /usr/bin
@@ -8,10 +8,10 @@ sudo ln -s $HOME/terraform terraform
 cd ~/oci-chef-apache
 
 #Get OCI Plugin for Terraform
-wget "https://github.com/oracle/terraform-provider-oci/releases/download/v2.0.6/linux.tar.gz"
+wget "https://github.com/oracle/terraform-provider-oci/releases/download/v2.1.5/linux.tar.gz"
 tar -xvzf linux.tar.gz
 #Add OCI to Terraform Plugins
-rm -rf linux_386/ linux_arm/ linux.tar.gz
+rm linux_amd64.tar.gz
 mkdir ~/.terraform.d
 mkdir ~/.terraform.d/plugins
 mv linux_amd64/ ~/.terraform.d/plugins/
